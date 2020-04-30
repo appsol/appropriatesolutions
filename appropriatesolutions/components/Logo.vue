5416e2dc204f9f9f84a6306a91e386e8540e2107
Stuart Laverick <appsol@gmail.com>
2020-04-30 09:17:12 +0100

    Moved to Nuxt.js

---
+++ appropriatesolutions/components/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <img class="logo" :style="logoStyle" src="~assets/images/logo.svg" />
+</template>
+<script>
+  export default {
+    props: {
+      width: {
+        type: Number,
+        default: 200
+      }
+    },
+    computed: {
+      logoStyle() {
+        return { width: this.width + 'px' }
+      }
+    }
+  }
+</script>
+<style>
+  .logo {
+    animation: 1s appear;
+    margin: auto;
+  }
+
+  @keyframes appear {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
---
+++ appropriatesolutions/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="hero is-info is-large">
+    <div class="hero-body">
+      <div class="container">
+        <div>
+          <logo />
+          <h1 class="title">appropriate solutions</h1>
+          <h2 class="subtitle">Web application development</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {
+      Logo
+    }
+  }
+</script>
+
+<style>
+</style>
