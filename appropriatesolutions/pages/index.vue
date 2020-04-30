d57a6f2d6ea116f7adf5e701d4d59178ad4a229b
Stuart Laverick <appsol@gmail.com>
2020-04-30 15:55:25 +0100

    Add .nojekyll to static for GH pages

--- appropriatesolutions/pages/index.vue
+++ appropriatesolutions/pages/index.vue
@@ -2,10 +2,20 @@
   <section class="hero is-info is-large">
     <div class="hero-body">
       <div class="container">
-        <div>
-          <logo />
-          <h1 class="title">appropriate solutions</h1>
-          <h2 class="subtitle">Web application development</h2>
+        <div class="level">
+          <div class="level-left">
+            <div class="level-item">
+              <logo />
+            </div>
+          </div>
+          <div class="level-right">
+            <div class="level-item">
+              <div>
+                <h1 class="title">appropriate solutions</h1>
+                <h2 class="subtitle">Web application development</h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
